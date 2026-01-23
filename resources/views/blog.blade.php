e5cb450deb28b86b025ab7501c04b3d52a249cd8
AlfianRamadhan89 <alfianramadhan3489@gmail.com>
2026-01-24 04:28:15 +0800

    bug

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -65,8 +65,4 @@
         <p class="text-center fs-4">No post found.</p>
     @endif
 
-    <div class="d-flex justify-content-end mt-3 mb-4">
-        {{ $posts->links() }}
-    </div>
-
 @endsection
